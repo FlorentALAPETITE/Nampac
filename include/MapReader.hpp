ad00a7b4fcda1a124303d505114240b032b813b8
Florent ALAPETITE <E166319G@I003V2pc17.ensinfo.sciences.univ-nantes.prive>
2016-11-03 13:54:51 +0100

    Added doxygen configuration and MapReader doc

--- include/MapReader.hpp
+++ include/MapReader.hpp
@@ -7,8 +7,27 @@
 
 using namespace std;
 
+
+/**
+ * \file MapReader.cpp
+ * \brief Labyrinth file reader.
+ * \author Florent ALAPETITE / Dorian LATOUCHE
+ *
+ * Static labyrinth file (.laby) reader;
+ *
+ */
+
+
 class MapReader{
 	public:	
+
+		/**
+		 * static vector<vector<int>> BuildMap(const char* file);
+		 * \brief Static method used to build an integer representation of the map contained in the file given.
+		 *
+		 * \param file .laby file containing an integer representation of a map.
+		 * \return Vector of vector of integer : map integer representation used to build a graphical labyrinth.
+		 */
 		static vector<vector<char>> BuildMap(const char* fichier);
 };
 
