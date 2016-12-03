7e968b773ce5f0fe5b4540a55e0ff521c9d89d17
Florent ALAPETITE <florent037@gmail.com>
2016-12-03 16:01:55 +0100

    Documentation multiple additions + Wall class little modification for the bonus setter

--- include/CharacterDecorator/SlowedCharacter.hpp
+++ include/CharacterDecorator/SlowedCharacter.hpp
@@ -3,10 +3,31 @@
 
 #include <CharacterDecorator/CharacterDecorator.hpp>
 
+/**
+ * \brief Decorator concrete implementation for the Nampac : SlowedCharacter.
+ * \author Florent ALAPETITE / Dorian LATOUCHE
+ *
+ * Decorator concrete class implementation for the Nampac game. The SlowedCharacter decorate a character and decrease its speed. Non-modified methods are calling the decorated object's method.
+ *
+ */
+
 class SlowedCharacter : public CharacterDecorator {
 
 	public:
-		SlowedCharacter(shared_ptr<Character>);
+		/**
+		 * SlowedCharacter(shared_ptr<Character> c);
+		 * \brief SlowedCharacter constructor.
+		 *		
+		 * \param c : the decorated character.
+		 */
+		SlowedCharacter(shared_ptr<Character> c);
+
+		/**
+		 * int getSpeed() override;	
+		 * \brief Decorated getter for the character speed.
+		 *		
+		 * \return int : 75% of the decorated character speed.
+		 */
 		int getSpeed() override;		
 	
 };
